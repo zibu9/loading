d2f48158b1572bc3d587dd228a60b625acc62351
Zibu <kabuyajunior082@gmail.com>
2024-03-29 22:38:13 +0100

    add keyframes spin css

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ gsap.fromTo('.loading-page0' , {opacity:1}, {
     delay:3.5,
 });
 
-
 gsap.fromTo('.logo-name' , {
     y:50,
     opacity:0,
@@ -13,4 +12,13 @@ gsap.fromTo('.logo-name' , {
     opacity:1,
     duration:2,
     delay:.5,
-})
+});
+
+window.addEventListener("load", function() {
+    document.querySelectorAll(".content").forEach(function(element) {
+        element.style.display = "block";
+    });
+    document.querySelectorAll(".loader-wrapper").forEach(function(element) {
+        element.style.display = "none";
+    });
+});
