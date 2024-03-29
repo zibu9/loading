3563390c34207b7fda7b384840677515ccacc9be
Zibu <kabuyajunior082@gmail.com>
2024-03-29 23:03:52 +0100

    finalize all

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const handlePageLoad = () => {
     document.querySelectorAll(".content").forEach(function(element) {
         element.style.display = "block";
     });
-    document.querySelector(".loading-page").style.display = "none";
+    document.querySelector(".loader-wrapper").style.display = "none";
 }
+
+
+//window.addEventListener("load", handlePageLoad);
 
 setTimeout(handlePageLoad, 3000);
